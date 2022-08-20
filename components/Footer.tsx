45624ca94e859f3d83945337ecf3e629f1605da5
Angelo Gulina <agulina@gitlab.com>
2022-08-20 14:14:03 +0200

    Update border and colors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,10 +6,15 @@ export const Footer = () => {
     <footer
       style={{
         background: "white",
-        borderTopColor: grey3,
         borderTopStyle: "solid",
-        borderTopWidth: 40,
-        padding: 20,
+        borderTopWidth: 6,
+        borderImageWidth: 6,
+        borderColor: grey3,
+        // borderImage:
+        //   "linear-gradient(to right, rgba(80,206,187,1) 30%, rgba(255,255,255,1) 30%, rgba(255,255,255,1) 70%, rgba(246,115,122,1) 70%) 1",
+        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
         position: "fixed",
         right: 0,
         bottom: 0,
