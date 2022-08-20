76360d930263f493e0f40dd5d721ddaabb544dcb
Angelo Gulina <agulina@gitlab.com>
2022-08-20 19:39:38 +0200

    Update how to read

--- components/HowToRead.tsx
+++ components/HowToRead.tsx
@@ -12,26 +12,36 @@ export const HowToRead = () => {
   return (
     <section>
       <HomeSectionTitle>Come leggere la tabella?</HomeSectionTitle>
-      <Text
+      <List
         className={homeSection}
         style={{
           margin: "0 auto 40px",
         }}
       >
-        {/*Ogni tema puo assumere uno di questi stati: -{" "}*/}
-        {/*<Image*/}
-        {/*  src="/endorsement/green.svg"*/}
-        {/*  alt="green"*/}
-        {/*  height={20}*/}
-        {/*  width={20}*/}
-        {/*/>{" "}*/}
-        {/*la lista si è espressa a favore del tema -{" "}*/}
-        {/*<Image src="/endorsement/red.svg" alt="green" height={20} width={20} />{" "}*/}
-        {/*la lista **non** si è espressa a favore del tema -{" "}*/}
-        {/*{" "}*/}
-      </Text>
-
-      <List>
+        <List.Item
+          icon={
+            <Image
+              src="/endorsement/green.svg"
+              alt="green"
+              height={20}
+              width={20}
+            />
+          }
+        >
+          il tema non è presente nelle fonti a nostra disposizione
+        </List.Item>
+        <List.Item
+          icon={
+            <Image
+              src="/endorsement/red.svg"
+              alt="green"
+              height={20}
+              width={20}
+            />
+          }
+        >
+          il tema non è presente nelle fonti a nostra disposizione
+        </List.Item>
         <List.Item
           icon={
             <Image
