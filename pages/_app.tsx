3f685271c90fbfb85bfa0e7f68e8ce0e9a154064
Angelo Gulina <agulina@gitlab.com>
2022-08-21 14:39:53 +0200

    Move FontAwesome init code to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,12 @@ import { Global, MantineProvider, Space } from "@mantine/core";
 import Head from "next/head";
 
 import { CustomFonts } from "../fonts";
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
