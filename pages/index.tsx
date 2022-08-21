5ed62466a8c284e402af77f5edcaaede5e63fc4d
Angelo Gulina <agulina@gitlab.com>
2022-08-21 11:38:50 +0200

    Prevent FontAwesome flashing out

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,13 @@ import { useRouter } from "next/router";
 import { CategoryData } from "../services/data";
 import { Main } from "../components/Main";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 interface Props extends DefaultProps {
   categories: CategoryData[];
 }
