28c64c3b8bcf27102bd75b2e9cb88918eb77d15a
Angelo Gulina <agulina@gitlab.com>
2022-08-21 14:49:31 +0200

    UI improvements on How to

--- components/HowToRead.tsx
+++ components/HowToRead.tsx
@@ -1,19 +1,36 @@
 import React from "react";
-import { List, Text } from "@mantine/core";
+import { createStyles, List, Text } from "@mantine/core";
 import Image from "next/image";
 import { onBoardingFontSize, useCommonStyles } from "../styles";
 import { HomeSectionTitle } from "./HomeSectionTitle";
+
+const useStyles = createStyles((theme) => ({
+  list: {
+    marginLeft: "30px !important",
+    [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+      marginLeft: "auto !important",
+      position: "relative",
+      left: 30,
+      textAlign: "left",
+      maxWidth: 490,
+    },
+  },
+}));
 
 export const HowToRead = () => {
   const {
     classes: { homeSection },
   } = useCommonStyles({});
 
+  const {
+    classes: { list },
+  } = useStyles();
+
   return (
     <section>
       <HomeSectionTitle>Come leggere la tabella?</HomeSectionTitle>
       <List
-        className={homeSection}
+        className={`${homeSection}, ${list}`}
         style={{
           margin: "0 auto 40px",
         }}
@@ -51,7 +68,7 @@ export const HowToRead = () => {
               fontSize: onBoardingFontSize,
             }}
           >
-            la lista si è espressa in opposizione del tema
+            la lista si è espressa in opposizione al tema
           </Text>
         </List.Item>
         <List.Item
