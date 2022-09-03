392c898201068691f18fce40b97bf14f09e2e478
Angelo Gulina <agulina@gitlab.com>
2022-09-03 19:30:45 +0200

    💁‍ Highlight contribute section

--- components/Contribute.tsx
+++ components/Contribute.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { createStyles, List, Text } from "@mantine/core";
-import Image from "next/image";
+import { Paper, Text } from "@mantine/core";
 import { onBoardingFontSize, useCommonStyles } from "../styles";
 import { HomeSectionTitle } from "./HomeSectionTitle";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export const Contribute = () => {
   const {
@@ -12,40 +9,50 @@ export const Contribute = () => {
   } = useCommonStyles({});
 
   return (
-    <section className={homeSection}>
-      <HomeSectionTitle>Come contribuisco al progetto?</HomeSectionTitle>
-      <Text
-        style={{
-          fontSize: onBoardingFontSize,
-        }}
-      >
-        {
-          "Le informazioni sono raccolte al meglio delle nostre capacità e in continuo aggiornamento, seguendo l'evoluzione della campagna elettorale."
-        }
-      </Text>
-      <Text>
-        <br />
-      </Text>
-      <Text
-        style={{
-          fontSize: onBoardingFontSize,
-        }}
-      >
-        Manca qualcosa? Hai trovato un errore? Compila{" "}
-        <a
-          href={
-            "https://docs.google.com/forms/d/e/1FAIpQLSe-34dHEjGaO3-8lc4JQDxdp7Yyditj9PycBL-lpARtTQPlGA/viewform"
-          }
-          target={"_blank"}
-          rel={"noreferrer"}
+    <Paper
+      p="md"
+      style={{
+        background: "#EAF5FA",
+        paddingTop: 30,
+        paddingBottom: 60,
+      }}
+    >
+      <section className={homeSection}>
+        <HomeSectionTitle>Come contribuisco al progetto?</HomeSectionTitle>
+        <Text
           style={{
-            textDecoration: "underline",
+            fontSize: onBoardingFontSize,
           }}
         >
-          questo form
-        </a>{" "}
-        (anonimo). Facci avere i tuoi suggerimenti.
-      </Text>
-    </section>
+          {
+            "Le informazioni sono raccolte al meglio delle nostre capacità e in continuo aggiornamento, seguendo l'evoluzione della campagna elettorale."
+          }
+        </Text>
+        <Text>
+          <br />
+        </Text>
+        <Text
+          style={{
+            fontSize: onBoardingFontSize,
+          }}
+        >
+          <strong>Manca qualcosa?</strong> Hai trovato un errore? Compila{" "}
+          <a
+            href={
+              "https://docs.google.com/forms/d/e/1FAIpQLSe-34dHEjGaO3-8lc4JQDxdp7Yyditj9PycBL-lpARtTQPlGA/viewform"
+            }
+            target={"_blank"}
+            rel={"noreferrer"}
+            style={{
+              color: "#009FC4",
+            }}
+          >
+            questo form
+          </a>{" "}
+          anonimo. <br />
+          Facci avere i tuoi suggerimenti.
+        </Text>
+      </section>
+    </Paper>
   );
 };
