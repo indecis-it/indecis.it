851e4b985d6abac18822625bffc0e4aa72c72b2d
Angelo Gulina <agulina@gitlab.com>
2022-09-06 07:32:54 +0200

    🫥  Temporarily hide collaboration on mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,30 +1,44 @@
-import { Group, Text } from "@mantine/core";
+import { createStyles, Group, Text } from "@mantine/core";
 import { grey3 } from "../colors";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
+const useStyles = createStyles((theme) => ({
+  footer: {
+    background: "white",
+    borderTopStyle: "solid",
+    borderTopWidth: 6,
+    borderImageWidth: 6,
+    borderColor: grey3,
+    padding: 10,
+    paddingLeft: 12,
+    paddingRight: 12,
+    [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+      paddingLeft: 60,
+      paddingRight: 60,
+      position: "fixed",
+      right: 0,
+      bottom: 0,
+      left: 0,
+      zIndex: 2000,
+    },
+  },
+  credits: {
+    display: "none",
+    [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+      display: "block",
+    },
+  },
+}));
+
 export const Footer = () => {
+  const { classes } = useStyles();
+
   return (
-    <footer
-      style={{
-        background: "white",
-        borderTopStyle: "solid",
-        borderTopWidth: 6,
-        borderImageWidth: 6,
-        borderColor: grey3,
-        padding: 10,
-        paddingLeft: 60,
-        paddingRight: 60,
-        position: "fixed",
-        right: 0,
-        bottom: 0,
-        left: 0,
-        zIndex: 2000,
-      }}
-    >
+    <footer className={classes.footer}>
       <Group position="apart">
-        <Text size={"sm"}>
+        <Text size={"sm"} className={classes.credits}>
           Con il supporto di{" "}
           <a
             target={"_blank"}
