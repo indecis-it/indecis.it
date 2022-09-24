9288c1ca684584b5a3433e0ebf88be6ab0a95286
Gabriele Scalici <gaba41@gmail.com>
2022-09-25 00:25:27 +0200

    fix: analytics

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -19,25 +19,6 @@ export const GoogleAnalytics = () => {
   return consent ? (
     <>
       <Script
-        id="ga-data-layer-prep"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag("consent", "default", {
-            ad_storage: "denied",
-            analytics_storage: "denied",
-            functionality_storage: "denied", // optional
-            personalization_storage: "denied", // optional
-            security_storage: "denied", // optional
-            wait_for_update: 2000 // milliseconds
-          });
-          gtag('set', 'url_passthrough', true);
-          gtag("set", "ads_data_redaction", true);
-          `,
-        }}
-      />
-      <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-WKG4HP07MS"
       />
